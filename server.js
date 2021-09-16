5caf355e103829c987082c7624a43ec421100975
martin <70706555+martinmara@users.noreply.github.com>
2021-09-16 15:45:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,16 @@ message.channel.send("pong")
 
 })
 
+Client.on("message", message => {
+
+if (message.content === "meme") {
+
+message.channel.send("https://www.google.com/search?q=meme&client=firefox-b-d&sxsrf=AOaemvKLqL72tJMLMWDAnt4nErnawrcsJA:1631799924988&tbm=isch&source=iu&ictx=1&fir=lLODzzlfHmoxSM%252C8LkjXcth-iXjoM%252C_&vet=1&usg=AI4_-kQcH3dkxYGk7sOpBaxM0j0E_HyfnA&sa=X&ved=2ahUKEwik5f220IPzAhUghf0HHfE9B_YQ9QF6BAgOEAE#imgrc=lLODzzlfHmoxSM")
+
+}
+
+})
+
 
 
 Client.login("")
